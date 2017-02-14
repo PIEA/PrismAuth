262572db3ef5752977a76d9111700d2ddce7f52e
Sepi <deet1022@gmail.com>
2017-02-14 20:02:33 +0900

    Add ContextConstants class

---
+++ PrismAuth/PrismAuth/Util/ContextConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MiNET.Utils;
+
+namespace PrismAuth.Util
+{
+    sealed class ContextConstants
+    {
+        public const string prefix = "[§6PrismAuth§f]";
+    }
+}
