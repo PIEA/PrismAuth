46e8280f4ca06792c436824a811cd1bc26592d9a
Sepi <deet1022@gmail.com>
2017-02-14 19:54:07 +0900

    Add PrismAuth class

---
+++ PrismAuth/PrismAuth/PrismAuth.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MiNET;
+using MiNET.Plugins;
+using MiNET.Plugins.Attributes;
+
+namespace PrismAuth
+{
+    [Plugin(Author = "Sepi", Description = "MiNET Auth plugin", 
+        PluginName = "PrismAuth", PluginVersion = "v0.1")]
+    public class PrismAuth : Plugin
+    {
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+        }
+
+        public override void OnDisable()
+        {
+            base.OnDisable();
+        }
+    }
+}
