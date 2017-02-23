7afc9ee76d622806b33b140b624b523bb6fffffb
Sepi <deet1022@gmail.com>
2017-02-23 14:36:27 +0900

    로그인 하지 않았을 시 움직이지 못하도록 추가

---
+++ PrismAuth/PrismAuth/Handler/PacketHandler/McpeMovePlayerPacket.cs
@@ -0,0 +1,28 @@
+﻿using MiNET;
+using MiNET.Net;
+using MiNET.Plugins.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrismAuth.Handler.PacketHandler
+{
+    public class McpeMovePlayerPacket : BasePacketHandler
+    {
+        [PacketHandler]
+        public void HandleMcpeMovePlayer(McpeMovePlayer packet, Player target)
+        {
+            if (!this.AccountManager.IsLogined(target))
+            {
+                target.SetPosition(new MiNET.Utils.PlayerLocation()
+                {
+                    X = target.SpawnPosition.X,
+                    Y = target.SpawnPosition.Y,
+                    Z = target.SpawnPosition.Z
+                }, false);
+            }
+        }
+    }
+}
