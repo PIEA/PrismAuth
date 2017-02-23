c4d047670aec27d50a3165161a5ef59c36a6d400
Sepi <deet1022@gmail.com>
2017-02-23 11:53:51 +0900

    Using문 제거

--- PrismAuth/PrismAuth/PrismAuth.cs
+++ PrismAuth/PrismAuth/PrismAuth.cs
@@ -1,19 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MiNET;
-using MiNET.Plugins;
+﻿using MiNET.Plugins;
 using MiNET.Plugins.Attributes;
-using System.IO;
-using PrismAuth.Util;
 using PrismAuth.Account;
-using Newtonsoft.Json;
-using PrismAuth.Commands;
-using PrismAuth.Handler;
-using PrismAuth.Handler.EventHandler;
-using PrismAuth.Handler.PacketHandler;
 
 namespace PrismAuth
 {
